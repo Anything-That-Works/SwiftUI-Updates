5b9308c94857cf23f16c5030b4f5c1c9012a7175
Anything-That-Works <87904517+Anything-That-Works@users.noreply.github.com>
2023-09-30 17:02:43 +0600

    Add files via upload
    Init

---
+++ SwiftUI updates (June 2023)/SwiftUI updates (June 2023)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI updates (June 2023)
+//
+//  Created by Promal on 30/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI updates (June 2023)/SwiftUI updates (June 2023)/SwiftUI_updates__June_2023_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_updates__June_2023_App.swift
+//  SwiftUI updates (June 2023)
+//
+//  Created by Promal on 30/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_updates__June_2023_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
